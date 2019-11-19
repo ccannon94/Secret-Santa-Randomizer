5e70f500a5756b89f47d8427d3ab816742485a06
Chris Cannon <chris.c.cannon@gmail.com>
2019-11-18 23:41:56 -0500

    Parameterize the subject line

--- SecretSantaRandomizer/src/secretsantarandomizer/SecretSantaRandomizer.java
+++ SecretSantaRandomizer/src/secretsantarandomizer/SecretSantaRandomizer.java
@@ -16,6 +16,7 @@ import java.util.logging.Logger;
 import javafx.util.Pair;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
@@ -27,11 +28,13 @@ import javax.mail.internet.MimeMessage;
  * @author CCannon
  */
 public class SecretSantaRandomizer {
+
     private static ArrayList<Pair> inputList;
     private static ArrayList<Pair> assignmentList;
-    
+
     private static String emailUserName;
     private static String emailPassword;
+
     /**
      * @param args the command line arguments
      */
@@ -39,27 +42,27 @@ public class SecretSantaRandomizer {
         inputList = new ArrayList<>();
         readList(args[0]);
         assignmentList = new ArrayList<>();
-        for(Pair pair : inputList) {
+        for (Pair pair : inputList) {
             assignmentList.add(pair);
         }
         randomizeAssignmentList();
         readEmailConfig(args[1]);
-        
-        String subject = "TKD Fam Secret Santa Assignment!";
-        for(int i = 0; i < inputList.size(); i++) {
+
+        String subject = args[2];
+        for (int i = 0; i < inputList.size(); i++) {
             String body = "Hey " + inputList.get(i).getKey() + "!"
                     + System.lineSeparator() + System.lineSeparator()
                     + "Your Secret Santa assignment is " + assignmentList.get(i).getKey() + "."
                     + System.lineSeparator() + System.lineSeparator()
-                    + "Shhhhh! Don't tell! See you in class!";
+                    + "Shhhhh! Don't tell!";
             sendFromGMail((String) inputList.get(i).getValue(), subject, body);
         }
     }
-    
+
     public static void readList(String filename) {
         try {
             Scanner reader = new Scanner(new File(filename));
-            while(reader.hasNext()) {
+            while (reader.hasNext()) {
                 String[] inputLine = reader.nextLine().split(",");
                 Pair<String, String> entry = new Pair<>(inputLine[0].trim(), inputLine[1].trim());
                 inputList.add(entry);
@@ -69,7 +72,7 @@ public class SecretSantaRandomizer {
             System.err.println("No list input file found!");
         }
     }
-    
+
     public static void readEmailConfig(String filename) {
         try {
             Scanner reader = new Scanner(new File(filename));
@@ -80,14 +83,14 @@ public class SecretSantaRandomizer {
             System.err.println("No email config input file found");
         }
     }
-    
+
     public static void randomizeAssignmentList() {
         boolean duplicatesPresent = true;
-        while(duplicatesPresent) {
+        while (duplicatesPresent) {
             Collections.shuffle(assignmentList);
             boolean duplicateFound = false;
-            for(int i = 0; i < inputList.size(); i++) {
-                if(inputList.get(i).getKey().equals(assignmentList.get(i).getKey())){
+            for (int i = 0; i < inputList.size(); i++) {
+                if (inputList.get(i).getKey().equals(assignmentList.get(i).getKey())) {
                     duplicateFound = true;
                 }
             }
@@ -96,35 +99,36 @@ public class SecretSantaRandomizer {
     }
 
     private static void sendFromGMail(String to, String subject, String body) {
-        Properties props = System.getProperties();
-        String host = "smtp.gmail.com";
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.user", emailUserName);
-        props.put("mail.smtp.password", emailPassword);
-        props.put("mail.smtp.port", "587");
+        //Get properties object    
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "587");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
-
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage message = new MimeMessage(session);
-
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.starttls.enable", "true");
+        //get Session   
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailUserName, emailPassword);
+            }
+        });
+        //compose message    
         try {
-            message.setFrom(new InternetAddress(emailUserName));
-            InternetAddress toAddress = new InternetAddress(to);
-            message.addRecipient(Message.RecipientType.TO, toAddress);
-
+            MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject(subject);
             message.setText(body);
+            //send message  
             Transport transport = session.getTransport("smtp");
-            transport.connect(host, emailUserName, emailPassword);
+            transport.connect(props.getProperty("mail.smtp.host"), emailUserName, emailPassword);
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
+            System.out.println("message sent successfully");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         }
-        catch (AddressException ae) {
-            ae.printStackTrace();
-        }
-        catch (MessagingException me) {
-            me.printStackTrace();
-        }
+
     }
 }
